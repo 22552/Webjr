b32764e572da6dc426e5ed3001e29eb9d5a2ce1b
22552 <arilris096@gmail.com>
2026-08-19 15:39:47 +0900

    Add IndexedDB-backed project storage

---
+++ src/tablet/WebForage.js
@@ -0,0 +1,110 @@
+import localforage from 'localforage';
+import WebUtils from './WebUtils';
+import {getSQLData, isEmptyOrSpaces} from './WebSQLMatch';
+import {ExecWebForageSQL} from './WebForageSQL';
+
+export default class WebForage {
+    constructor (userName) {
+        this.userName = userName;
+        this.tables = {};
+        this.tableDes = {
+            projects: {PRIMARY: 'id', ISGIFT: {DEFAULT: 0}},
+            usershapes: {PRIMARY: 'id'},
+            userbkgs: {PRIMARY: 'id'},
+            userfiles: {PRIMARY: 'name'},
+            test: {PRIMARY: 'id'}
+        };
+        Object.keys(this.tableDes).forEach(tableName => {
+            this.tables[tableName] = localforage.createInstance({
+                name: 'Webjr-' + this.userName,
+                storeName: tableName
+            });
+        });
+    }
+
+    getTablePrimary (tableName) {
+        const desc = this.tableDes[tableName];
+        return desc && desc.PRIMARY;
+    }
+
+    initTableDefault (tableName, key, obj) {
+        const desc = this.tableDes[tableName] || {};
+        const primary = desc.PRIMARY;
+        if (primary && obj[primary] == null && !isEmptyOrSpaces(String(key))) obj[primary] = key;
+        Object.keys(desc).forEach(name => {
+            const rule = desc[name];
+            if (name !== 'PRIMARY' && rule && rule.DEFAULT != null && obj[name.toLowerCase()] == null) {
+                obj[name.toLowerCase()] = rule.DEFAULT;
+            }
+        });
+    }
+
+    setItem (tableName, key, obj, fcn) {
+        const store = this.tables[tableName];
+        if (!store) return fcn(-1, null);
+        this.initTableDefault(tableName, key, obj);
+        store.setItem(String(key), obj).then(value => fcn(0, value)).catch(err => {
+            WebUtils.log(err);
+            fcn(-2, null);
+        });
+    }
+
+    updateItem (tableName, key, patch, fcn) {
+        this.getItem(tableName, key, (code, oldObj) => {
+            if (code !== 0) return fcn(code, null);
+            const next = Object.assign({}, oldObj || {}, patch);
+            this.setItem(tableName, key, next, fcn);
+        });
+    }
+
+    getItem (tableName, key, fcn) {
+        const store = this.tables[tableName];
+        if (!store) return fcn(-1, null);
+        store.getItem(String(key)).then(value => fcn(0, value)).catch(err => {
+            WebUtils.log(err);
+            fcn(-2, null);
+        });
+    }
+
+    deleteItem (tableName, key, fcn) {
+        const store = this.tables[tableName];
+        if (!store) return fcn(-1, null);
+        store.removeItem(String(key)).then(() => fcn(0, null)).catch(err => {
+            WebUtils.log(err);
+            fcn(-2, null);
+        });
+    }
+
+    getAllItems (tableName, fcn) {
+        const store = this.tables[tableName];
+        if (!store) return fcn(-1, null);
+        const items = [];
+        store.iterate(value => { items.push(value); }).then(() => fcn(0, items)).catch(err => {
+            WebUtils.log(err);
+            fcn(-2, null);
+        });
+    }
+
+    deleteAllItems (tableName, fcn) {
+        const store = this.tables[tableName];
+        if (!store) return fcn(-1, null);
+        store.clear().then(() => fcn(0, null)).catch(err => {
+            WebUtils.log(err);
+            fcn(-2, null);
+        });
+    }
+
+    exec (body, fcn) {
+        const json = typeof body === 'string' ? JSON.parse(body) : body;
+        const sqlData = getSQLData(json.stmt, json.values || []);
+        ExecWebForageSQL(sqlData, this, fcn);
+    }
+
+    stmt (body, fcn) {
+        this.exec(body, (code, value) => fcn(code === 0 ? (typeof value === 'string' ? value : true) : null));
+    }
+
+    query (body, fcn) {
+        this.exec(body, (code, list) => fcn(code === 0 ? list : null));
+    }
+}
